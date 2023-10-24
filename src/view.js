2be0f56d066a6e2c298181bd7bf19d9cae99a420
LuciaDuranD <oluciadurandiaz@gmail.com>
2023-10-24 10:04:00 -0500

    actualizacion del formato de diseño queda pendiente los personajes que todo desde 0

--- src/view.js
+++ src/view.js
@@ -1,12 +1,47 @@
 export const renderItems = (data) => {
-  console.log(data);
-  // Aquí comienza tu código y puedes retornar lo que tu necesites
-  return "example";
+  const ulElement = document.getElementById("root");
+
+  data.forEach((personaje) => {
+    const liElement = document.createElement("li");
+    liElement.setAttribute("itemscope", "itemscope");
+    liElement.setAttribute("itemtype", "https://schema.org/Person");
+
+    const imgElement = document.createElement("img");
+    imgElement.setAttribute("src", personaje.ImagenSrc);
+    imgElement.setAttribute("alt", personaje.Nombre);
+
+    const dtNombre = document.createElement("dt");
+    dtNombre.textContent = "Nombre:";
+
+    const ddNombre = document.createElement("dd");
+    ddNombre.setAttribute("itemprop", "name");
+    ddNombre.textContent = personaje.Nombre;
+
+    const dtDescripcion = document.createElement("dt");
+    dtDescripcion.textContent = "Descripción:";
+
+    const ddDescripcion = document.createElement("dd");
+    ddDescripcion.setAttribute("itemprop", "description");
+    ddDescripcion.textContent = personaje.Edad;
+
+    liElement.appendChild(imgElement);
+    liElement.appendChild(dtNombre);
+    liElement.appendChild(ddNombre);
+    liElement.appendChild(dtDescripcion);
+    liElement.appendChild(ddDescripcion);
+
+    ulElement.appendChild(liElement);
+  });
 };
 
-const mostrarMenu = document.getElementById("mostrar-menu");
-const menuOculto = document.querySelector(".menu-oculto");
+const personajes = [
+  {
+    Nombre: "Rick Grimes",
+    Edad: "Ex sheriff y líder del grupo de supervivientes.",
+    ImagenSrc: "./Imagenes/Rick.jpg",
+    Estado: "Vivo",
+  },
+  //FALTA COLOCAR EL RESTO DE PERSONAJES LIZZ//
+];
 
-mostrarMenu.addEventListener("click", () => {
-  menuOculto.classList.toggle("menu-oculto-activo");
-});
+renderItems(personajes);
