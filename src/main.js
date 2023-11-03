dac89d9a726ad0cfc595df44f87adfc1156bf097
LuciaDuranD <oluciadurandiaz@gmail.com>
2023-11-03 14:09:03 -0500

    errores

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
+import { filtroLet0, computeStats } from "./dataFunctions.js";
 import { renderItems } from "./view.js";
-import data from "./data/dataset.js"; // Importa tus datos solo una vez
+import data from "./data/dataset.js";
 
-document.addEventListener("DOMContentLoaded", function () {
-  console.log(renderItems(data), data);
-});
+const losPersonajes = document.getElementById("root");
+losPersonajes.appenChild(renderItems(data));
+
+const filtradoLetras = document.querySelector('[id="filtro-letra"]');
+const filtradoEdades = document.querySelector('[id="filtro-edad"]');
+const filtradoEstados = document.querySelector('[id="filtro-estado"]');
+
+filtradoLetras.addEventListener("change", filtrados);
+filtradoEdades.addEventListener("change", filtrados);
+filtradoEstados.addEventListener("change", filtrados);
+
+function filtrados() {
+  const indiceLetras = filtradoLetras.selectedIndex;
+  const optionSelect = filtradoLetras.options[indiceLetras];
+  const indiceEdades = filtradoEdades.selectedIndex;
+  const optionSelectEdades = filtradoEdades.options[indiceEdades];
+  const indiceEstados = filtradoEstados.selectedIndex;
+  const optionSelectEstados = filtradoEstados.options[indiceEstados];
+  losPersonajes.innerHTML = "";
+}
--- src/view.js
+++ src/view.js
@@ -1,19 +1,60 @@
 export const renderItems = (data) => {
-  console.log("Datos recibidos:", data);
+  const losUl = document.createElement("ul");
+  losUl.setAttribute("class", "cajas-personajes");
 
-  const container = document.getElementById("personajes-list");
-  data.forEach((personaje) => {
-    const personajeDiv = document.createElement("dl");
-    personajeDiv.setAttribute("itemscope", "");
-    personajeDiv.setAttribute("itemtype", "https://schema.org/Person");
-    personajeDiv.innerHTML = `
-    <img src="${personaje.imageUrl}" alt="${personaje.id}">
-    <dt>Nombre:</dt><dd itemprop="name">${personaje.name}</dd>
-    <dt>Edad:</dt><dd itemprop="age">${personaje.facts.age}</dd>
-    <dt>Estado:</dt><dd itemprop="status">${personaje.facts.status}</dd>
-    <dt>Descripción:</dt><dd itemprop="description">${personaje.description}</dd>
-`;
-    container.appendChild(personajeDiv);
-  });
-  return container;
+  for (const p of data) {
+    const losLi = document.createElement("li");
+    losLi.setAttribute("class", "lista");
+    losLi.setAttribute("itemscope", "");
+    losLi.setAttribute("itemtype", "https://schema.org/Person");
+
+    losUl.appendChild(losLi);
+
+    const losDl = document.createElement("dl");
+    losLi.appendChild(losDl);
+
+    losDl.setAttribute("itemscope", "");
+    losDl.setAttribute("itemtype", "https://schema.org/Person");
+
+    const imagenes = document.createElement("img");
+    img.src = personaje.imageUrl;
+    img.alt = personaje.id;
+
+    //NOMBRES
+    const elDt1 = document.createElement("dt");
+    elDt1.textContent = "NOMBRE:";
+    losDl.appendChild(elDt1);
+    const elDd1 = document.createElement("dd");
+    losDl.appendChild(elDd1);
+    elDd1.setAttribute("itemprop", "name");
+    elDd1.textContent = p.name;
+
+    //EDADES
+    const elDt2 = document.createElement("dt");
+    elDt2.textContent = "EDAD:";
+    losDl.appendChild(elDt2);
+    const elDd2 = document.createElement("dd");
+    losDl.appendChild(elDd2);
+    elDd2.setAttribute("itemprop", "age");
+    elDd2.textContent = p.facts.age;
+
+    //ESTADOS Vivos/Muertos
+    const elDt3 = document.createElement("dt");
+    elDt3.textContent = "ESTADO:";
+    losDl.appendChild(elDt3);
+    const elDd3 = document.createElement("dd");
+    losDl.appendChild(elDd3);
+    elDd3.setAttibute("itemprop", "status");
+    elDd3.textContent = p.facts.status;
+
+    //DESCRIPCIONES
+    const elDt4 = document.createElement("dt");
+    elDt4.textContent = "DESCRIPCIÓN:";
+    losDl.appendChild(elDt4);
+    const elDd4 = document.createElement("dd");
+    losDl.appendChild(elDd4);
+    elDd4.setAttribute("itemprop", "description");
+    elDd4.textContent = p.description;
+  }
+  return losUl;
 };
