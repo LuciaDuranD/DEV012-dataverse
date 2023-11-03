ceee8544402417cace79fbc2a3a0b4a7ec16ab5d
Liasilva95 <amaosilvalizbeth.1303@gmail.com>
2023-11-03 12:05:10 -0500

    avance filtros y addEvenListener

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,5 +1,75 @@
 // Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
 
+
+export const filterData = (data, filterBy, value) => {
+//Creamos un array vacio para almacenar los resultados del filtro
+const filteredData= [];
+
+// Recorremos cada tarjeta de los personajes en los datos
+for (const personaje of data) {
+//Verificamos si el campo 'filterBy' existe en la tarjeta de personaje
+if (personaje.hasOwnProperty(filterBy)) {
+//Verificamos si el valor del campo 'filterBy' coincide con el valor deseado
+if (personaje[filterBy]=== value) {
+ //Si coincide, agregamos la tarjeta de personaje al array filtrado
+ filteredData.push(personaje);
+}
+}
+}
+//Devolver el array filtrado
+return filteredData;
+};
+
+export const sortData = (data, sortBy, sortOrder) => {
+ // Comprobamos si el campo sortBy existe en al menos una tarjeta de personaje
+ if (data.length === 0 || !data[0].hasOwnProperty(sortBy)) {
+  return data; // No se puede ordenar, devuelve los datos sin cambios
+}
+
+// Clonamos los datos originales para evitar modificar el array original
+const clonedData = [...data];
+
+// Función de comparación para la ordenación
+const compareFunction = (a, b) => {
+  if (a[sortBy] < b[sortBy]) {
+    return sortOrder === 'asc' ? -1 : 1;
+  }
+  if (a[sortBy] > b[sortBy]) {
+    return sortOrder === 'asc' ? 1 : -1;
+  }
+  return 0;
+};
+
+// Ordena el array clonado según el campo y el orden especificado
+clonedData.sort(compareFunction);
+
+return clonedData;
+}
+
+export const computeStats = data.reduce (
+  (acc, personaje) => {
+    // Incrementar el contador de personajes
+    acc.totalPersonajes++;
+
+    // Sumar la edad de los personajes
+    acc.totalEdad += personaje.edad;
+
+    // Encontrar la edad máxima
+    acc.maxEdad = Math.max(acc.maxEdad, personaje.edad);
+
+    // Encontrar la edad mínima
+    acc.minEdad = Math.min(acc.minEdad, personaje.edad);
+
+    return acc;
+  },
+  {
+    totalPersonajes: 0,
+    totalEdad: 0,
+    maxEdad: Number.NEGATIVE_INFINITY, // Inicializar con el valor mínimo posible
+    minEdad: Number.POSITIVE_INFINITY, // Inicializar con el valor máximo posible
+  }
+)
+
 export const example = () => {
   return "example";
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,56 @@
+import { filterData, sortData, computeStats } from "./dataFunctions.js";
 import { renderItems } from "./view.js";
+
 import data from "./data/dataset.js"; // Importa tus datos solo una vez
 
 document.addEventListener("DOMContentLoaded", function () {
   console.log(renderItems(data), data);
 });
+
+let result = data;
+const rootRender = document.querySelector("#root");
+rootRender.appendChild(renderItems(result));
+const filtroLetra = document.getElementById("filtro-letra");
+const filtroEdad = document.getElementById("filtro-edad");
+const filtroEstado = document.getElementById("filtro-estado");
+const menu = document.getElementById("menu");
+const closeButton = document.getElementById("close-button");
+
+function showMenu() {
+  menu.style.transform = "translateX(0)";
+}
+
+function hideMenu() {
+  menu.style.transform = "translateX(-100%)";
+}
+
+filtroLetra.addEventListener("change", (e) => {
+  const filteredLetra = e.target.value;
+  dataset = filterData(data, "letra", filteredLetra);
+
+  listContent.innerHTML = "";
+  listContent.appendChild(renderItems(dataset));
+  statsParagraph.innerHTML = `"Total results: " + stats(dataset, filteredLetra)`;
+});
+
+filtroEdad.addEventListener("change", (e) => {
+  const filteredEdad = e.target.value;
+  dataset = filterData(data, "edad", filteredEdad);
+
+  listContent.innerHTML = "";
+  listContent.appendChild(renderItems(dataset));
+  statsParagraph.innerHTML = `"Total results: " + stats(dataset, filteredEdad)`;
+});
+
+filtroEstado.addEventListener("change", (e) => {
+  const filteredEstado = e.target.value;
+  dataset = filterData(data, "estado", filteredEstado);
+
+  listContent.innerHTML = "";
+  listContent.appendChild(renderItems(dataset));
+  statsParagraph.innerHTML = `"Total results: " + stats(dataset, filteredEstado)`;
+});
+
+menuButton.addEventListener("click", showMenu);
+
+closeButton.addEventListener("click", hideMenu);
